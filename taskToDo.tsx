3dfbc98e9a9e46b084caa8fadd3c9ea19060714b
manishprasad7173@gmail.com <manishprasad7173@gmail.com>
2025-02-25 01:22:44 +0530

    Remove unused taskToDo component and related comments

--- taskToDo.tsx
+++
@@ -1,6 +0,0 @@
-// variant = like ram , rom input field 
-// color is not coming in the frontend
-// address field
-// different gadgets pages 
-// my order page is full left
-// delete button to delete products
