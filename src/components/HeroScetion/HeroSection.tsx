4844ad72298d8ad72217ab053f1b871d89f45c5c
manishprasad7173@gmail.com <manishprasad7173@gmail.com>
2025-04-27 22:37:49 +0530

    feat: wrap button in Link component for improved navigation to product gallery

--- src/components/HeroScetion/HeroSection.tsx
+++ src/components/HeroScetion/HeroSection.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import { ImagesSlider } from "../ui/images-slider";
+import Link from 'next/link';
 
 
 function HeroSection() {
@@ -30,10 +31,14 @@ function HeroSection() {
             <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
               The hero section slideshow <br /> nobody asked for
             </motion.p>
-            <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-              <span>Start Shopping →</span>
-              <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-            </button>
+            <Link href="/productsgallery" passHref>
+              <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4"
+            
+              >
+                <span>Start Shopping →</span>
+                <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+              </button>
+            </Link>
           </motion.div>
           
          </ImagesSlider>
