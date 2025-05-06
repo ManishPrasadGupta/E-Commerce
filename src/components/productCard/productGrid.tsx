176aa20169affe04c9289c4458d28ba5f8def432
manishprasad7173@gmail.com <manishprasad7173@gmail.com>
2025-05-06 17:27:01 +0530

    feat: update ProductGrid component to enhance responsive layout for product display

--- src/components/productCard/productGrid.tsx
+++ src/components/productCard/productGrid.tsx
@@ -11,10 +11,13 @@ export default function ProductGrid({
     return (
       <div className={`flex flex-wrap justify-center gap-2 sm:gap-4 p-2 sm:p-4 ${className}`}>
         {products.map((product) => (
-          <div className="w-1/6" key={product._id?.toString() || "default-key"}>
+          <div 
+            className="w-[calc(50%-0.5rem)] sm:w-[calc(33.333%-1rem)] md:w-[calc(33.333%-1rem)] lg:w-[calc(25%-1rem)] xl:w-[calc(16.666%-1rem)]" 
+            key={product._id?.toString() || "default-key"}
+          >
             <ProductCard product={product} />
           </div>
         ))}
       </div>
-    );
+    ); 
   }
